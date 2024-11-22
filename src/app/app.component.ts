064fe5993d13c8152fee25150047dad3e0b8c6a7
Eric Mulvihill <eric.mulvihill@gmail.com>
2024-11-22 17:19:25 -0500

    Change to dark background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from "@angular/core";
 import {RouterOutlet} from "@angular/router";
 import {CommonModule} from "@angular/common";
 import {MatButton} from "@angular/material/button";
-import {MatCard, MatCardContent} from "@angular/material/card";
 import {MolViewComponent} from "./component/molview/molview.component";
 
 import {ContextInfo, MolViewRenderMode, MolViewSelectionMode} from "wglmolview";
@@ -17,7 +16,7 @@ import {AtomInfoComponent} from "./component/atom_info/atom-info.component";
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatButton, MatCard, MolViewComponent, MatLabel, MatSelect, MatOption, MatFormField, FormsModule, AtomInfoComponent],
+  imports: [CommonModule, RouterOutlet, MatButton, MolViewComponent, MatLabel, MatSelect, MatOption, MatFormField, FormsModule, AtomInfoComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
